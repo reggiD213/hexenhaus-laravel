f312bca8fddd611d2878871042e4d026c55e84d7
reggid213 <reggiD213@gmail.com>
2018-01-06 13:18:12 +0100

    added fritz logo, temporary commented andreas hessner out

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -26,12 +26,13 @@
             <div class="box ads">
                 <h3>Unsere Partner:</h3>
                 <hr>
-                <!--<a target="_blank" href="http://www.gold-ochsen.de/"><img src="/images/goldochsen.png" alt="Gold Ochsen" style="width: calc(30% - 20px)"></a>-->
+                {{--<a target="_blank" href="http://www.gold-ochsen.de/"><img src="/images/goldochsen.png" alt="Gold Ochsen" style="width: calc(30% - 20px)"></a>--}}
                 <a target="_blank" href="http://www.bergbier.de/"><img src="/images/berg.png" alt="Berg Brauerei" style="width: calc(30% - 20px)"></a>
                 <a target="_blank" href="http://www.kopierland.de/"><img src="/images/kopierland.png" alt="Kopierland" style="width: calc(70% - 20px)"></a>
-                <!--<a target="_blank" href="http://www.gamperling-veranstaltungstechnik.de/"><img src="/images/gamperling.png" alt="Gamperling Veranstaltungstechnik" style="width: calc(50% - 20px)"></a>-->
-                <a target="_blank" href="https://www.google.com/maps/place/KS+Getr%C3%A4nke+GmbH/@48.40772,9.992531,15z/data=!4m2!3m1!1s0x0:0x6cb389b7475023fe"><img src="/images/ksgetraenke.png" alt="K+S Getränke" style="width: calc(70% - 20px)"></a>
-                <a target="_blank" href="http://www.andreashessner.de"><img src="/images/andreashessner.png" alt="Andreas Hessner" style="width: calc(30% - 20px)"></a>
+                {{--<a target="_blank" href="http://www.gamperling-veranstaltungstechnik.de/"><img src="/images/gamperling.png" alt="Gamperling Veranstaltungstechnik" style="width: calc(50% - 20px)"></a>--}}
+                <a target="_blank" href="https://www.google.com/maps/place/KS+Getr%C3%A4nke+GmbH/@48.40772,9.992531,15z/data=!4m2!3m1!1s0x0:0x6cb389b7475023fe"><img src="/images/ksgetraenke.png" alt="K+S Getränke" style="width: calc(50% - 20px)"></a>
+                {{--<a target="_blank" href="http://www.andreashessner.de"><img src="/images/andreashessner.png" alt="Andreas Hessner" style="width: calc(30% - 20px)"></a>--}}
+                <a target="_blank" href="http://www.fritz-kola.de/"><img src="/images/fritz-kola_weiss_.png" alt="Fritz Kola" style="width: calc(50% - 20px)"></a>
             </div>
         </div>
     </div>
