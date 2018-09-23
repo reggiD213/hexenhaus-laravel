5b45da96419b473dace4ce9273f74229e7562968
Puddy <Puddy@hexenhaus-metal.de>
2018-09-23 15:56:50 +0200

    next try tidying up...

--- app/Helpers/bladeHelpers.php
+++ app/Helpers/bladeHelpers.php
@@ -3,3 +3,15 @@
 function set_active($path) {
     return call_user_func_array('Request::is', (array)$path) ? 'active' : '';
 }
+
+function html_tidy($src){
+    $opentag = strrchr($src, '<');
+    $closetag = strrchr($src, '>');
+
+    if ( ( $closetag  ||  $opentag ) ) {
+        if ( $closetag && (strlen($closetag) > strlen($opentag) )) {
+            $src = substr($src , 0 , -strlen($opentag));
+        }
+    }
+    return $src;
+}
